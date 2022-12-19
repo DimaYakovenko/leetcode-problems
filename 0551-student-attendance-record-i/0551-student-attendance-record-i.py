15fb1feb863aa94b476d8700cae629ef02a79743
Dmytro Yakovenko <dima.gemini@gmail.com>
2022-12-19 22:56:51 +0200

    Time: 34 ms (85.66%), Space: 13.8 MB (96.25%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,5 +1,14 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        if s.count('A') > 1 or 'LLL' in s:
-            return False
+        abs_count = 0
+        late_count = 0
+        for i in s:
+            if i == 'A':
+                abs_count += 1
+            if i == 'L':
+                late_count += 1
+            else: 
+                late_count = 0
+            if abs_count > 1 or late_count == 3:
+                return False
         return True
