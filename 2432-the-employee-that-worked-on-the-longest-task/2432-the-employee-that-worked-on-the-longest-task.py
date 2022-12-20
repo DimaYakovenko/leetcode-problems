df11b8f4246763ffba8924192219c1f76d1a3d1f
Dmytro Yakovenko <dima.gemini@gmail.com>
2022-12-20 17:13:03 +0200

    Time: 594 ms (61.65%), Space: 14 MB (22.76%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
@@ -1,15 +1,14 @@
 class Solution:
     def hardestWorker(self, n: int, logs: List[List[int]]) -> int:
-        start_prev = 0
-        emp_id_to_dur = dict()
-        for log in logs:
-            emp_id = log[0]
-            end_time = log[1]
-            duration = end_time - start_prev
-            start_prev = end_time
-            
-            if emp_id not in emp_id_to_dur or duration > emp_id_to_dur[emp_id]:
-                emp_id_to_dur[emp_id] = duration
-        emp_id_to_dur = sorted(emp_id_to_dur.items(), key=lambda i: (-i[1], i[0]) )
-        return emp_id_to_dur[0][0]
+        max_id = logs[0][0]
+        max_dur = logs[0][1]
+        last_end = logs[0][1]
+        for i in range(1, len(logs)):
+            if logs[i][1] - last_end > max_dur:
+                max_id = logs[i][0]
+                max_dur = logs[i][1] - last_end
+            elif logs[i][1] - last_end == max_dur:
+                max_id = min(max_id, logs[i][0])
+            last_end = logs[i][1]
+        return max_id
         
