ae00562ac372dd390b9f611013d74733c31bd1ea
Dmytro Yakovenko <dima.gemini@gmail.com>
2022-12-20 17:15:46 +0200

    Time: 879 ms (18.02%), Space: 14 MB (61.38%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
@@ -2,13 +2,11 @@ class Solution:
     def hardestWorker(self, n: int, logs: List[List[int]]) -> int:
         max_id = logs[0][0]
         max_dur = logs[0][1]
-        last_end = logs[0][1]
         for i in range(1, len(logs)):
-            if logs[i][1] - last_end > max_dur:
+            if logs[i][1] - logs[i-1][1] > max_dur:
                 max_id = logs[i][0]
-                max_dur = logs[i][1] - last_end
-            elif logs[i][1] - last_end == max_dur:
+                max_dur = logs[i][1] - logs[i-1][1]
+            elif logs[i][1] - logs[i-1][1] == max_dur:
                 max_id = min(max_id, logs[i][0])
-            last_end = logs[i][1]
         return max_id
         
